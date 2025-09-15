07274e44b0be9021b49a102889fb14ba6ced8670
Amal Shadin A <165491653+amalshadin@users.noreply.github.com>
2025-09-15 22:20:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/weather-flappy-bird",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
